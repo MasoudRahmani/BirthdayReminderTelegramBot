0bdfb820142820005a11928ddcbae646cf4c6d0f
Masoud Rahmani <mrem71@gmail.com>
2022-07-06 19:20:26 +0430

    Schedule to run at 8:30 AM

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { HappyBot } = require('./bot');
 const http = require('http');
+const schedule = require('node-schedule');
 
 /* Heroku something*/
 const server = http.createServer((rq, rs) => { rs.writeHead(200); rs.end('I am running dude.'); });
@@ -12,6 +13,16 @@ const testGr = -1001632481272
 const client_email = process.env.email;
 const private_key = process.env.key.replace(/\\n/g, "\n")
 
-let bot = new HappyBot(token, AWgroup)
 
-bot.run(userXcelSrc, client_email, private_key);
+let counter = 0
+const rule = new schedule.RecurrenceRule();
+rule.hour = 8; //new schedule.Range(0,23,2); //every 2hour
+rule.minute = 30;
+rule.tz = 'IR';
+console.log(["First Run is at:", rule.nextInvocationDate()].join(" "));
+
+let runner = schedule.scheduleJob(rule, () => {
+    let bot = new HappyBot(token, AWgroup)
+    bot.run(userXcelSrc, client_email, private_key);
+    console.log([++counter, "-", "Run at:", new Date()].join(" "));
+});
