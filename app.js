3a990dd34d86d13118b5d5953d0375add3141c29
Masoud Rahmani <mrem71@gmail.com>
2022-07-08 01:31:17 +0430

    exception and new method

--- app.js
+++ app.js
@@ -11,18 +11,22 @@ const userXcelSrc = process.env.document
 const AWgroup = process.env.groupId
 const client_email = process.env.email;
 const private_key = process.env.key.replace(/\\n/g, "\n")
-
-
 let counter = 0
+
+
+try{
 const rule = new schedule.RecurrenceRule();
 rule.hour = 11; //new schedule.Range(0,23,2); //every 2hour
 rule.minute = 30;
 
 console.log(["First Run is at:", rule.nextInvocationDate()].join(" "));
 
-let bot = new HappyBot(token, AWgroup)
+let bot = new HappyBot(token, AWgroup,userXcelSrc, client_email, private_key)
 
 let runner = schedule.scheduleJob(rule, () => {    
-    bot.run(userXcelSrc, client_email, private_key);
+    bot.SendHBD();
     console.log([++counter, "-", "Run at:", new Date()].join(" "));
 });
+} catch(error){
+    console.log(["Main Entry Err:", error.message.substring(0, 100), "..."].join(" "))
+}
