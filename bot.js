0ff743e94e35a20f891b6157c742cd7b5f331e9a
Masoud Rahmani <mrem71@gmail.com>
2022-07-08 01:30:55 +0430

    better resource handling

--- bot.js
+++ bot.js
@@ -9,20 +9,30 @@ class HappyBot {
     #bot;
     #jday = GetShamsiDay(new Date());
     #jMonth = GetShamsiMonth(new Date());
-    #gDocument
+    #gDocument;
+    #SheetSrc;
+    #gMail;
+    #gKey;
     /**
      * 
      * @param {string} authentication token 
      * @param {Integer} groupId to send happy birthday to 
+     * @param {String} Online Google SpreadSheet Src which has user birthdays on first sheet 
+     * @param {String} Google Service Key Authentication Email
+     * @param {String} Google Service Key Authentication Private Key
      */
-    constructor(token, groupToNotify) {
+    constructor(token, groupToNotify, sheetSrc, gEmail, gPrvKey) {
         this.#token = token;
         this.#prvGroup = groupToNotify;
-        this.#bot = new TelegramBot(this.#token, { polling: true });
+        this.#SheetSrc = sheetSrc;
+        this.#gMail = gEmail;
+        this.#gKey = gPrvKey;
         this.#botConfig();
     }
 
-    #botConfig() {
+    async #botConfig() {
+        this.#bot = new TelegramBot(this.#token, { polling: true });
+
         this.#bot.on('polling_error', (error) => {
             console.log(["Pulling Err:", error.message.substring(0, 100), "..."].join(" ")); // => 'EFATAL'
         });
@@ -30,27 +40,26 @@ class HappyBot {
             this.#bot.sendMessage(x.from.id, 'این بات پاسخگو به درخواستی نمی‌باشد.\n باتشکر').catch(x => this.handleSentErro(x));
         });
 
+        this.#gDocument = await this.#init(this.#SheetSrc, this.#gMail, this.#gKey);
     }
 
-    #init = async function (sheetSrc, email, key) {
-        const doc = new GoogleSpreadsheet(sheetSrc);
+    #init = async function (src, mail, key) {
+        const doc = new GoogleSpreadsheet(src);
         await doc.useServiceAccountAuth({
-            client_email: email,
+            client_email: mail,
             private_key: key
         });
         await doc.loadInfo();
         return doc;
     }
     /**
-    * @patam {String} Online Google SpreadSheet Src which has user birthdays on first sheet 
-    * @patam {String} Google Service Key Authentication Email
-    * @patam {String} Google Service Key Authentication Private Key
-    */
-    async run(sheetSrc, gEmail, gPrvKey) {
-        this.#gDocument = await this.#init(sheetSrc, gEmail, gPrvKey);
+     * Get AW Birthdays and Sent Happy Birthday to users which have aged today.
+     * @returns false if nothing happend, true if something happend
+     */
+    async SendHBD() {
         let photo = await this.#getBirthDayPhoto();
         let was_sent = await this.#wasTodaySent();
-        if (was_sent) return;
+        if (was_sent) return false;
 
         try {
             this.#getOnlineBirthdays().then(u => {
@@ -70,17 +79,20 @@ class HappyBot {
                 })
                 this.#LogSentCelebration(celbrated);
             })
+            return true;
         } catch (error) {
             this.#LogError(["Overall sent Err:", error.message.substring(0, 100), "..."].join(" "))
+            return false;
         }
     }
     async #wasTodaySent() {
         let today = MiladiToShamdiConvertor(new Date());
+        //let Yesterday = MiladiToShamdiConvertor(new Date(Date.now() - 86400000))
         let sent = false;
         let sheet = this.#gDocument.sheetsById[946533461];
         let rows = await sheet.getRows();
         rows.forEach(r => {
-            if (r.RunDate == today & r.Sent == 'TRUE') { sent = true; return; }
+            if (r.RunDate == today & !r.Error) { sent = true; return; }
         })
         return sent;
     }
@@ -108,7 +120,7 @@ class HappyBot {
         }
     }
     #getOnlineBirthdays = async function () {
-        let sheet = (await this.#gDocument).sheetsById[0];
+        let sheet = this.#gDocument.sheetsById[0];
         return await sheet.getRows();
     }
     async #getBirthDayPhoto() {
