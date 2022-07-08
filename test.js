1093c45ff2102f976101d8ec9db6540c6d7e6af4
Masoud Rahmani <mrem71@gmail.com>
2022-07-08 19:18:38 +0430

    test

--- test.js
+++ test.js
@@ -1,33 +1,39 @@
 'use strict';
 
-const TelegramBot = require('node-telegram-bot-api');
+// import * as TelegramBot from 'node-telegram-bot-api';
+import { HappyBot } from './bot.js';
+import schedule from 'node-schedule';
+import * as util from './my_util.js'
+import { GoogleSpreadsheet } from 'google-spreadsheet';
 
-
-const { HappyBot } = require('./bot');
-const schedule = require('node-schedule');
+import tgresolve from 'tg-resolve'
 
 const token = ""
 const userXcelSrc = ""
-const testGr = "-1001632481272"
+//const AWgroup = "-1001224485894"
 const client_email = ""
 const private_key = ""
 
+//let Yesterday = util.MiladiToShamdiConvertor(new Date(Date.now() - 86400000))
 
-app();
+//app();
 //just_tg();
-
+//resolver();
+//google();
 function app() {
     let counter = 0
     const rule = new schedule.RecurrenceRule();
-    rule.second = new schedule.Range(0, 59, 10); //every 1 minute;
+    rule.minute = new schedule.Range(0, 59, 1); //every 10 sec
 
     console.log(["First Run is at:", rule.nextInvocationDate()].join(" "));
 
-    let bot = new HappyBot(token, testGr, userXcelSrc, client_email, private_key)
+    let bot = new HappyBot(token, userXcelSrc, client_email, private_key)
+
 
     let runner = schedule.scheduleJob(rule, () => {
-        bot.SendHBD();
-        console.log([++counter, "-", "Run at:", new Date()].join(" "));
+    bot.SendHBD();
+    console.log(`${++counter} - Run at: ${new Date()}.
+      next run at: ${rule.nextInvocationDate()}`);
     });
 
 }
@@ -40,4 +46,31 @@ function just_tg() {
     function handleSentErro(error) {
         console.log(error.message)
     }
+}
+async function google() {
+    const doc = new GDoc.GoogleSpreadsheet(userXcelSrc);
+
+    await doc.useServiceAccountAuth({
+        client_email: client_email,
+        private_key: private_key
+    });
+    await doc.loadInfo();
+    let sheet = doc.sheetsById[946533461];
+}
+
+function resolver() {
+    // using the 'bare' function
+
+    // tgres(token, "@Masoud_rah", function (error, result) {
+    //     console.log(result.id);
+    // });
+
+    // you can create a client (referred to as 'resolver')
+    // that you can repeatedly use
+    const resolver = new tgresolve.Tgresolve(token);
+
+    // using the 'resolver'
+    resolver.tgresolve("@Masoud_rah", function (error, result) {
+        console.log(result.id);
+    });
 }
