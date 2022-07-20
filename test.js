7c8b5267e3fa98ef628977777373743f17240d3a
Masoud Rahmani <mrem71@gmail.com>
2022-07-20 20:31:03 +0430

    few more test and some less

--- test.js
+++ test.js
@@ -1,57 +1,60 @@
 'use strict';
 
-// import * as TelegramBot from 'node-telegram-bot-api';
+import TelegramBot from 'node-telegram-bot-api';
 import { HappyBot } from './bot.js';
 import schedule from 'node-schedule';
 import * as util from './my_util.js'
 import { GoogleSpreadsheet } from 'google-spreadsheet';
+import fs from 'fs/promises'
 
-import tgresolve from 'tg-resolve'
+class booba {
+    b(f) {
+        this._f = f;
+        t
+    }
+    async get() {
+        return await fs.readFile('HBD.jpg');
+    }
+}
 
-// const TG_Token = ""
-// const GoogleSheetID = ""
-// //const AWgroup = "-1001224485894"
-// const GoogleServiceAcc = ""
-// const GoogleKey = ""
+const TG_Token = ""
+const GoogleSheetID = ""
+const GoogleServiceAcc = ""
+const GoogleKey = ""
 
-//let Yesterday = util.MiladiToShamdiConvertor(new Date(Date.now() - 86400000))
-//console.log(`🌹🌹 🥳 بات تبریک تولد 💃🌹🌹`);
-
-app();
+Main();
 //just_tg();
-//resolver();
 //google();
-function app() {
-    let counter = 0
+
+
+async function Main() {
+
     try {
-        let bot = new HappyBot(TG_Token, GoogleSheetID, GoogleServiceAcc, GoogleKey)
+        let counter = 0
+        let bot = new HappyBot(TG_Token, GoogleSheetID, GoogleServiceAcc, GoogleKey);
 
+        await bot.Init();
+
+
+        const rule = new schedule.RecurrenceRule();
+       // rule.hour = new schedule.Range(0, 23, 1); //every 1hour
+       // rule.minute = 0 // needed for every 
+       rule.second = new schedule.Range(0,59,20);
        
-    const rule = new schedule.RecurrenceRule();
-    rule.hour = 4;//new schedule.Range(0, 23, 4); //every 4hour
-    rule.minute = 2 // needed for every 
+       console.log(`First run is at: ${rule.nextInvocationDate()}`);
 
-
-
-        console.log(`First run is at: ${rule.nextInvocationDate()}`);
-        let date = new Date();
-       // let runner = schedule.scheduleJob(rule, () => {
-
-            date.setHours(date.getHours() + 1);
+        let runner = schedule.scheduleJob(rule, () => {
+            let date = new Date();
             console.log(`${++counter} - Run at: ${date}.
-            next run at: ${rule.nextInvocationDate()}`);
-            console.log(date.getHours());
-            if (date.getHours() > 3 & date.getHours() < 20) {
-                bot.SendHBD();
-                console.log('yes');
-            }
-       // });
+        next run at: ${rule.nextInvocationDate()}`);
+            bot.SendHBD();
+        });
+        console.log(`First run is at: ${runner.nextInvocation()}`);
+        
     } catch (error) {
         console.log(`Main Entry Err: ${error.message.substring(0, 100)}...`);
     }
-
 }
-
 function just_tg() {
     let b = new TelegramBot(token, { polling: true });
     b.sendMessage(testGr, "test").catch(x => handleSentErro(x));
@@ -62,29 +65,20 @@ function just_tg() {
     }
 }
 async function google() {
-    const doc = new GDoc.GoogleSpreadsheet(userXcelSrc);
+    const doc = new GoogleSpreadsheet(GoogleSheetID);
 
     await doc.useServiceAccountAuth({
-        client_email: client_email,
-        private_key: private_key
+        client_email: GoogleServiceAcc,
+        private_key: GoogleKey
     });
     await doc.loadInfo();
     let sheet = doc.sheetsById[946533461];
 }
+//let Yesterday = util.MiladiToShamdiConvertor(new Date(Date.now() - 86400000))
 
-function resolver() {
-    // using the 'bare' function
-
-    // tgres(token, "@Masoud_rah", function (error, result) {
-    //     console.log(result.id);
-    // });
-
-    // you can create a client (referred to as 'resolver')
-    // that you can repeatedly use
-    const resolver = new tgresolve.Tgresolve(token);
-
-    // using the 'resolver'
-    resolver.tgresolve("@Masoud_rah", function (error, result) {
-        console.log(result.id);
-    });
-}
+let _b = new booba('test');
+(async function () { /* Top level Async */
+    // _p = await _b.get();
+    // console.log(_p.length);
+    // let _p;
+}());
